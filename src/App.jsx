ea4c2980abb1b9e6bf423d3d64abfd925aa7abaf
anastasijamijaljevic <anastasijamijaljevic11@gmail.com>
2025-11-01 00:19:34 +0100

    add Sidebar and navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import { Header } from './components/Header/Header'
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 function App() {
-  
+
+  const Home = () => <h2>Home Page</h2>;
+  const Anime = () => <h2>Anime Page</h2>;
+  const Manga = () => <h2>Manga Page</h2>;
+  const About = () => <h2>About Us</h2>;
+  const Favorites = () => <h2>Favorites</h2>;
 
   return (
-    <>
-     <Header/>
-    </>
+
+    <Router>
+      <Header />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/anime' element={<Anime />} />
+        <Route path='/manga' element={<Manga />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/favorites' element={<Favorites />} />
+
+      </Routes>
+
+    </Router>
+
+
   )
 }
 
